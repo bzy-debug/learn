10330d5cb873b95abd58e68c78062ff55b4c6dc5
bzy-debug <bzy_sustech@foxmail.com>
2022-11-15 09:47:59 +0800

    up

---
+++ vue/vue-complete-course/02/basics-assignment-1-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: "Bao Zhiyuan",
+      age: 22,
+      image: "https://image.gcores.com/463509b7-b28f-428b-8ab8-1b6994a24a3b.jpg?x-oss-process=image/resize,limit_1,m_fill,w_956,h_536/quality,q_90/format,webp"
+    }
+  }
+})
+
+app.mount('#assignment')
