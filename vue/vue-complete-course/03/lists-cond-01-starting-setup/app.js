84d78e8ba9be29855355cc663dc12c4d3b308aea
bzy-debug <bzy_sustech@foxmail.com>
2022-11-16 01:22:35 +0800

    up

---
+++ vue/vue-complete-course/03/lists-cond-01-starting-setup/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      goals: [],
+      inputGoal: '',
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.inputGoal)
+    }
+  }
+});
+
+app.mount('#user-goals');
