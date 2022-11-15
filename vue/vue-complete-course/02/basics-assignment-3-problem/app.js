6a413f75f6256419d069b01f9df8dffcd598d064
bzy-debug <bzy_sustech@foxmail.com>
2022-11-15 16:16:36 +0800

    up

---
+++ vue/vue-complete-course/02/basics-assignment-3-problem/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  watch: {
+    counter() {
+      const that = this
+      setTimeout(() => {
+        that.counter = 0
+      }, 5000);
+    }
+  },
+  computed: {
+    result() {
+      if (this.counter === 37) {
+        return 'Exactly'
+      }
+      return this.counter < 37 ? 'Not there yet' : 'Too much!'
+    }
+  },
+  methods: {
+    add(num) {
+      this.counter += num
+    }
+  },
+})
+
+app.mount('#assignment')
