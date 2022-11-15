03e3c737caf96b651701591cc4acfc9d717bef92
bzy-debug <bzy_sustech@foxmail.com>
2022-11-15 10:49:52 +0800

    up

---
+++ vue/vue-complete-course/02/basics-03-events-starting-code/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.counter ++
+    },
+    remove() {
+      if(this.counter > 0)
+        this.counter --
+    }
+  }
+});
+
+app.mount('#events');
