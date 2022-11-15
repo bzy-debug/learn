d35930055a35aaef3386cc505e06b7b62ec6fd19
bzy-debug <bzy_sustech@foxmail.com>
2022-11-15 11:01:20 +0800

    up

--- vue/vue-complete-course/02/basics-03-events-starting-code/app.js
+++ vue/vue-complete-course/02/basics-03-events-starting-code/app.js
@@ -2,9 +2,14 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      inputName: '',
     };
   },
   methods: {
+    setName(e) {
+      this.inputName = e.target.value
+    },
+
     add(num) {
       this.counter += num
     },
