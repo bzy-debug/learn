83e9b7d3c181801de8a2ce40dd2ce0a2d6bf34e8
bzy-debug <bzy_sustech@foxmail.com>
2022-11-19 23:54:49 +0800

    up

---
+++ vue/vue-complete-course/07/first-vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ vue/vue-complete-course/07/first-vue-app/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetail">{{ toggleText }}</button>
+    <ul v-show="detailVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailVisible: false,
+      friend: {
+        id: 1,
+        name: "wqq",
+        phone: "1234567890",
+        email: "wqq@localhost.com",
+      },
+    };
+  },
+  computed: {
+    toggleText() {
+      return (this.detailVisible ? "Hide" : "Show") + " Details";
+    },
+  },
+  methods: {
+    toggleDetail() {
+      this.detailVisible = !this.detailVisible;
+    },
+  },
+};
+</script>
