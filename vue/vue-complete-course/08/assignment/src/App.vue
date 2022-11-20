d4cce328175161b29ef67dc6eb807728d45e69d6
bzy-debug <bzy_sustech@foxmail.com>
2022-11-20 20:59:07 +0800

    up

---
+++ vue/vue-complete-course/08/assignment/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>User Data Assginment</h1>
+  <hr />
+  <user-data @new-user="addUser"></user-data>
+  <active-user
+    v-for="userData in userDatas"
+    :key="userData.id"
+    :name="userData.name"
+    :age="userData.age"
+  ></active-user>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userDatas: [],
+    };
+  },
+  methods: {
+    addUser(newUser) {
+      newUser.id = new Date().toISOString();
+      this.userDatas.push(newUser);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ vue/vue-complete-course/08/assignment/src/components/UserData.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="addUser">
+    <div>
+      <label>Name: </label>
+      <input type="text" v-model="name" placeholder="User Name" />
+    </div>
+    <div>
+      <label>Age: </label>
+      <input type="number" v-model="age" />
+    </div>
+    <button>Add User</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["new-user"],
+  data() {
+    return {
+      name: "",
+      age: 0,
+    };
+  },
+  methods: {
+    addUser() {
+      this.$emit("new-user", { name: this.name, age: this.age });
+    },
+  },
+};
+</script>
