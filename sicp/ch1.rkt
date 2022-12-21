0e079268d08b459074f11cb6cf08949983005e4d
bzy-debug <bzy_sustech@foxmail.com>
2022-12-21 15:01:04 +0800

    up

--- sicp/ch1.rkt
+++ sicp/ch1.rkt
@@ -334,3 +334,13 @@
           (try next))))
   (try first-guess))
 
+(define (double f)
+  (lambda (x) (f (f x))))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (compose f (repeated f (- n 1)))))
